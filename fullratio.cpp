b1789199d27d9c0457db56de68529984c982c53d
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-06 17:59:00 +0200

    Create fullratio.cpp

---
+++ fullratio.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#ifndef CONTEST
+#include "fullratio.hpp"
+#endif
+#include <cmath>
+
+using namespace std;
+
+rational::rational (int n, int d)
+{
+nom=n;
+den=d;
+}
+
+rational operator+(const rational &x,const rational &y)
+{
+int r1=(x.nom*y.den)+(y.nom*x.den);
+int r2=x.den*y.den;
+int k=gcd(r1,r2);
+r1=r1/k;
+r2=r2/k;
+if(r1<0 && r2<0)
+{
+r1=abs(r1);
+r2=abs(r2);
+}
+return(rational(r1,r2));
+}
+
+
+rational operator-(const rational &x,const rational &y)
+{
+int r1=(x.nom*y.den)-(y.nom*x.den);
+int r2=x.den*y.den;
+int k=gcd(r1,r2);
+r1=r1/k;
+r2=r2/k;
+if(r1<0 && r2<0)
+{
+r1=abs(r1);
+r2=abs(r2);
+}
+return(rational(r1,r2));
+}
+
+rational operator*(const rational &x,const rational &y)
+{
+int r1=(x.nom*y.nom);
+int r2=x.den*y.den;
+int k=gcd(r1,r2);
+r1=r1/k;
+r2=r2/k;
+if(r1<0 && r2<0)
+{
+r1=abs(r1);
+r2=abs(r2);
+}
+return(rational(r1,r2));
+}
+
+
+rational operator/(const rational &x,const rational &y)
+{
+int r1=(x.nom*y.den);
+int r2=x.den*y.nom;
+int k=gcd(r1,r2);
+r1=r1/k;
+r2=r2/k;
+if(r1<0 && r2<0)
+{
+r1=abs(r1);
+r2=abs(r2);
+}
+return(rational(r1,r2));
+}
+
+
+int rational::gcd(int a,int b)
+{
+a = abs(a); b = abs(b);
+ while (a > 0 && b > 0)
+ if (a > b) a = a % b; else b = b % a;
+ return(a+b);
+}
+
+ostream& operator<<(ostream &out, const rational &r)
+{
+out<<r.nom<<"/"<<r.den<<" ";
+return out;
+}
+
+
